dcfe84598f2c8ff53c52301687f78b374b357837
samuel ricky <komputerindo21@gmail.com>
2019-10-08 12:07:25 +0700

    Update seeder

--- laravelweb_PSy/database/seeds/ShiftsTableSeeder.php
+++ laravelweb_PSy/database/seeds/ShiftsTableSeeder.php
@@ -15,6 +15,7 @@ class ShiftsTableSeeder extends Seeder
     {
         $faker = Faker::create('id_ID');
         $today = Carbon::today()->format('Y-m-d');
+        $todayBeforeThisMonth = Carbon::today()->subDays(30)->format('Y-m-d');
 
         // for($i = 0;$i < 5;$i++) //disetiap waktu
         // {
@@ -49,7 +50,7 @@ class ShiftsTableSeeder extends Seeder
                 'room_id' => 1,
                 'time_id' => 1,
                 'date' => $today,
-                'status_node_id' => mt_rand(1,3),
+                'status_node_id' => 1,
                 'message' => 'Aman pak !', 
                 'scan_time' => '',
             ]);
@@ -59,9 +60,9 @@ class ShiftsTableSeeder extends Seeder
                 'room_id' => 2,
                 'time_id' => 1,
                 'date' => $today,
-                'status_node_id' => mt_rand(1,3),
+                'status_node_id' => 1,
                 'message' => 'Sepertinya ada pencuri !', 
-                'scan_time' => '',
+                'scan_time' => '12:10:10',
             ]);
 
         DB::table('shifts')->insert([
@@ -69,13 +70,42 @@ class ShiftsTableSeeder extends Seeder
                 'room_id' => 2,
                 'time_id' => 3,
                 'date' => $today,
-                'status_node_id' => mt_rand(1,3),
+                'status_node_id' => 2,
+                'message' => 'Mencurigakan pak !', 
+                'scan_time' => '',
+            ]);
+        DB::table('shifts')->insert([
+                'user_id' => 1,
+                'room_id' => 1,
+                'time_id' => 1,
+                'date' => $todayBeforeThisMonth,
+                'status_node_id' => 2,
+                'message' => 'Aman pak !', 
+                'scan_time' => '10:10:10',
+            ]);
+
+        DB::table('shifts')->insert([
+                'user_id' => 1,
+                'room_id' => 2,
+                'time_id' => 1,
+                'date' => $todayBeforeThisMonth,
+                'status_node_id' => 2,
+                'message' => 'Sepertinya ada pencuri !', 
+                'scan_time' => '10:10:10',
+            ]);
+
+        DB::table('shifts')->insert([
+                'user_id' => 1,
+                'room_id' => 2,
+                'time_id' => 3,
+                'date' => $todayBeforeThisMonth,
+                'status_node_id' => 1,
                 'message' => 'Mencurigakan pak !', 
                 'scan_time' => '',
             ]);
 
 
-    	for($i = 1; $i <= 50; $i++){    		
+    	for($i = 1; $i <= 100; $i++){    		
     		DB::table('shifts')->insert([
     			'user_id' => mt_rand(1,25),
     			'room_id' => mt_rand(1,10),
--- laravelweb_PSy/database/seeds/UsersTableSeeder.php
+++ laravelweb_PSy/database/seeds/UsersTableSeeder.php
@@ -66,7 +66,7 @@ class UsersTableSeeder extends Seeder
         //random generate
         $faker = Faker::create('id_ID');
         
-    	for($i = 1; $i <= 15; $i++){    	
+    	for($i = 1; $i <= 30; $i++){    	
             $randomString = '';
             for ($j = 0; $j < 16; $j++) {
                 $randomString .= $characters[rand(0, $charactersLength - 1)];
