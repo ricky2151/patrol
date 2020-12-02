1846ec154687a78df1d5f153cc03d6b43bc96e88
Ricky <samuel.ricky@ti.ukdw.ac.id>
2020-12-02 21:35:24 +0700

    Refactor auth controller
    use auth service

--- laravelweb_PSy/app/Http/Controllers/AuthController.php
+++ laravelweb_PSy/app/Http/Controllers/AuthController.php
@@ -2,20 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\User;
+//use Illuminate\Http\Request;
 use App\Http\Requests\ValidateLogin;
 use Illuminate\Support\Facades\Auth;
-use Tymon\JWTAuth\Facades\JWTAuth;
-use Tymon\JWTAuth\Exceptions\JWTException;
+// use Tymon\JWTAuth\Facades\JWTAuth;
+// use Tymon\JWTAuth\Exceptions\JWTException;
+use App\Services\Contracts\AuthServiceContract as AuthService;
 
 
 
 class AuthController extends Controller
 {
-    
+    protected $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
     public function login(ValidateLogin $request)
     {
+        $this->authService->login($request);
         try {
 
             $credentials['username'] = $request->username;
@@ -92,36 +99,6 @@ class AuthController extends Controller
     {
         return $this->respondWithToken(auth()->refresh());
     }
-    // public function isLogin()
-    // {
-    //     //dd(Auth::user());
-    //     $user = Auth::user();
-    //     if(Auth::check()){
-    //         return response()->json([
-    //             'error' => false,
-    //             'isLogin' => true,
-    //             'user' => $user,
-    //         ]);
-    //     }
-    //     else
-    //     {
-    //         return response()->json([
-    //             'error' => true,
-    //             'isLogin' => false,
-    //             'user' => $user
-    //         ]);
-    //     }
-    // }
 
-    // public function logout()
-    // {
-    //     auth()->logout();
-    //     return response()->json([
-    //         'error' => false,
-    //         'message' => 'Logout Success',
-    //     ]);
-
-    	
-    // }
 
 }
