89932de0c1913e3cc5c9ac4cd9b9c63a451e03d5
Ricky <samuel.ricky@ti.ukdw.ac.id>
2020-12-27 16:51:00 +0700

    Update roleadmin middleware to use authservice

--- laravelweb_PSy/app/Http/Middleware/RoleAdmin.php
+++ laravelweb_PSy/app/Http/Middleware/RoleAdmin.php
@@ -5,9 +5,17 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Services\Contracts\AuthServiceContract as AuthService;
+use App\Exceptions\LoginFailedException;
 
 class RoleAdmin
 {
+    protected $authService;
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
     /**
      * Handle an incoming request.
      *
@@ -17,20 +25,33 @@ class RoleAdmin
      */
     public function handle($request, Closure $next)
     {
+        //harusnya manggil AuthService.checkToken()
+        //karena kalau misal ada error, maka tar uda
+        //otomatis ke throw errornya
 
-    	if(Auth::check())
-    	{
-    		$user = User::find(auth()->user()->id);
-    		if($user->canPlayARole('Admin'))
-    		{
-    			return $next($request);	
-    		}
-    	}
+        if($this->authService->canMePlayARoleAsAdmin())
+        {
+            return $next($request);
+        }
+        else
+        {
+            throw new LoginFailedException("You Are Not Admin !");
+        }
+        
+    	// if(Auth::check())
+    	// {
+    	// 	$user = User::find(auth()->user()->id);
+    	// 	if($user->canPlayARole('Admin'))
+    	// 	{
+    				
+    	// 	}
+        // }
+        // else
+        // {
+        //     dd($x);
+        // }
 
-        return response()->json([
-            'error' => true,
-            'message' => 'Your are not admin',
-        ]);
+        
         	
         
     }
