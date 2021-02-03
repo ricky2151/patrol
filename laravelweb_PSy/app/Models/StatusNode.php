1ec9292a8f1f33af8db3caf4d68d7601981b09c0
Ricky <samuel.ricky@ti.ukdw.ac.id>
2021-02-03 10:11:01 +0700

    Hide created_at and deleted_at on status_nodes

--- laravelweb_PSy/app/Models/StatusNode.php
+++ laravelweb_PSy/app/Models/StatusNode.php
@@ -8,6 +8,7 @@ class StatusNode extends Model
 {
 	protected $table = 'status_nodes';
     protected $fillable = ['name']; 
+    protected $hidden = ['created_at', 'updated_at'];
     public function histories()
     {
     	return $this->hasMany('App\Models\History');
