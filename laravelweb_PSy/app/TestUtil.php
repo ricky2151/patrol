9bf21366b46f52f5e4250073c02ff7120bdcdd77
Ricky <samuel.ricky@ti.ukdw.ac.id>
2021-02-13 11:15:33 +0700

    Update mock logic in test util

--- laravelweb_PSy/app/TestUtil.php
+++ laravelweb_PSy/app/TestUtil.php
@@ -18,22 +18,41 @@ class TestUtil{
                     $mockThisMethod = false;
                 }
                 if($mockThisMethod) {
+                    
+                    $times = 1;
+                    if($methods[$i]['returnOrThrow'] === null) {
+                        $times = 0;
+                    } else {
+                        if(isset($methods[$i]['times'])) {
+                            $times = $methods[$i]['times'];
+                        } 
+                    }
+                    
+
                     if(!($methods[$i]['returnOrThrow'] instanceof \Exception))
                     {
                         if(isset($methods[$i]['usingParam'])) {
-                            $mock->shouldReceive($methods[$i]['method'])->with($methods[$i]['usingParam'])->andReturn($methods[$i]['returnOrThrow']);
+                            $mock->shouldReceive($methods[$i]['method'])->with($methods[$i]['usingParam'])
+                            ->andReturn($methods[$i]['returnOrThrow'])
+                            ->times($times);
                         }
                         else {
-                            $mock->shouldReceive($methods[$i]['method'])->andReturn($methods[$i]['returnOrThrow']);
+                            $mock->shouldReceive($methods[$i]['method'])
+                            ->andReturn($methods[$i]['returnOrThrow'])
+                            ->times($times);
                         }
                     }
                     else
                     {
                         if(isset($methods[$i]['usingParam'])) {
-                            $mock->shouldReceive($methods[$i]['method'])->with($methods[$i]['usingParam'])->andThrow($methods[$i]['returnOrThrow']);
+                            $mock->shouldReceive($methods[$i]['method'])->with($methods[$i]['usingParam'])
+                            ->andThrow($methods[$i]['returnOrThrow'])
+                            ->times($times);
                         }
                         else {
-                            $mock->shouldReceive($methods[$i]['method'])->andThrow($methods[$i]['returnOrThrow']);
+                            $mock->shouldReceive($methods[$i]['method'])
+                            ->andThrow($methods[$i]['returnOrThrow'])
+                            ->times($times);
                         }
                         
                     } 
